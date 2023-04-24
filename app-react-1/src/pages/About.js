8beaef024d6efaea38cf14737a0a0f67264eb0b7
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-04-24 17:16:37 +0200

    edit

---
+++ app-react-1/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from '../components/Nav/navigation';
+
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ipsa incidunt! Quas praesentium at modi voluptates magni optio numquam distinctio, beatae hic? Obcaecati hic perspiciatis numquam blanditiis, aliquid alias fuga.
+
+            </p>
+        </div>
+    );
+};
+
+export default About;
---
+++ app-react-1/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Nav/navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>ACCUEIL</h1>
+        </div>
+    );
+};
+
+export default Home;
